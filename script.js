81639142261e9ab3dbeefea86162ace83cfaf05a
Masiel Bautista <masiel5@users.noreply.github.com>
2020-07-13 21:01:51 -0400

    edited script code

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowerCases = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCases = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -17,11 +16,9 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
 // Prompts for password criteria
 function generatePassword() {
   var passwordLength = prompt("Choose a number of characters for your password. It must be at least 8 characters and no more than 128 characters.");
-  
   
 // Variables  
 var passwordLength = "";
@@ -30,13 +27,13 @@ var confirmLowerCases;
 var confirmUpperCases;
 var confirmSpecialCharacters;
   
-  //Check to make sure user selected ok for all and uses empty minimums from above
-  if (passwordLength <= 7 || passwordLength >= 129) {
-    alert("Password length must be between 8-50 characters. Try again please.");
+//Setting up criteria
+  if (passwordLength <= 7 || passwordLength >= 129 ) {
+    alert("Password length must be between 8-128 characters. Please try again");
     var passwordLength = (prompt("Choose a number of characters for your password. It must be at least 8 characters and no more than 128 characters."));
-  }
-    alert(`Your password will have ${passwordLength} characters`); {
-    
+    }  
+    alert(`Your password will have ${passwordLength} characters`); 
+  {
     var confirmLowerCases = confirm("Do you want lowercases in your password?");
     var confirmUpperCases = confirm("Do you want uppercases in your password?");
     var confirmNumbers = confirm("Do you want numbers in your password?");
